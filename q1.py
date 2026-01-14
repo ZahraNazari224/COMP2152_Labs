5daff72d153db9e4bcd524a643c4ddf964b683a7
ZahraNazari224 <znazari595@gmail.com>
2026-01-14 17:18:50 -0500

    First Commit

---
+++ q1.py
@@ -0,0 +1,24 @@
+# Sample Coding Question 01 Week 01
+# Zahra Nazari
+
+
+numbers=[1,4,7,9]      # List of numbers
+
+
+a=1
+b=2
+c=3
+d=4
+
+e=a*c-b/d
+e=(a*c)-(b/d)
+e=a-b**c//d+a%c
+e=(a-((b**c)//d))+(a%c)
+
+
+temperature=32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+userAge = int(input("Enter your age: "))
+userAge+=22
+print("Now showing the shop items filtered by age ", userAge)
